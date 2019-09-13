44c5a41c152dc445836cdc6850ae42e17adb2585
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2019-09-12 21:56:34 -0300

    Commit 2 Diseño Index

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+//Declaraciones init
+
+function initials(){   	
+    setTimeout(function(){
+          $(".titleslogan").fadeIn("slow"); 
+          $(".sdas").fadeIn("slow");   
+        }, 1000);
+    
+    console.log("dajsd");
+}
