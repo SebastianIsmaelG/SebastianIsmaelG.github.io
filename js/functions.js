fb3524d884ebeaecb963da764cc24b032a5b12f8
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2019-09-24 18:23:44 -0300

    Mitad de index completo

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
 //Declaraciones init
-
-function initials(){   	
-    setTimeout(function(){
-          $(".titleslogan").fadeIn(); 
-            
-        }, 1000);
-    
-    console.log("dajsd");
+function muestra_oculta(id){
+   if (document.getElementById){ //se obtiene el id
+   var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+   el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+   }
+}       
+   window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+   muestra_oculta('contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
 }
 
 function maquina(contenedor,texto,intervalo){
